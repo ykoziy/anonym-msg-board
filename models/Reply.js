aa365a855b09cd95d01f415b7ad8a47803e4dca3
Yuriy Koziy <yukoziy@gmail.com>
2019-12-07 08:06:41 -0500

    Added password hashing to Thread and Reply models.

--- models/Reply.js
+++ models/Reply.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 let ReplySchema = new Schema({
@@ -7,5 +8,26 @@ let ReplySchema = new Schema({
   created_on:       {type: Date, default: Date.now},
   reported:         {type: Boolean, default: false}
 });
+
+ReplySchema.pre('save', function(next) {
+  if (!this.isModified('delete_password')) {
+    return next();
+  }
+  return bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(this.delete_password, salt, (err, hash) => {
+      if (err) return next(err);
+        this.delete_password = hash;
+        return next();
+    });
+  });
+});
+
+ReplySchema.methods.comparePassword = function(password, cb) {
+  bcrypt.compare(password, this.delete_password, (err, res) => {
+    if (err) return cb(err);
+    cb(null, res);
+  });
+};
 
 module.exports = mongoose.model("Reply", ReplySchema);
--- models/Thread.js
+++ models/Thread.js
@@ -1,15 +1,36 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 let ThreadSchema = new Schema({
   text:             String,
-  delete_password:  String, //temp solution, should be shashed
+  delete_password:  String,
   created_on:       {type: Date, default: Date.now},
   bumped_on:        {type: Date, default: Date.now},
   reported:         {type: Boolean, default: false},
   replies:          {type: Schema.Types.ObjectId, ref: 'Reply'}
 });
 //// Store board title???????
-/// Recomend res.redirect to board page /b/{board}
+
+ThreadSchema.pre('save', function(next) {
+  if (!this.isModified('delete_password')) {
+    return next();
+  }
+  return bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(this.delete_password, salt, (err, hash) => {
+      if (err) return next(err);
+        this.delete_password = hash;
+        return next();
+    });
+  });
+});
+
+ThreadSchema.methods.comparePassword = function(password, cb) {
+  bcrypt.compare(password, this.delete_password, (err, res) => {
+    if (err) return cb(err);
+    cb(null, res);
+  });
+};
 
 module.exports = mongoose.model('Thread', ThreadSchema);
