a4138c0de9e293f436c7103e37623a577a33199d
Yuriy Koziy <yukoziy@gmail.com>
2019-12-11 12:05:25 -0500

    Fixed field threads, since it should be an array.

--- models/Board.js
+++ models/Board.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 let BoardSchema = new Schema({
   board_title:     String,
-  threads:  {type: Schema.Types.ObjectId, ref: 'Thread'}
+  threads:  [{type: Schema.Types.ObjectId, ref: 'Thread'}]
 });
 
 module.exports = mongoose.model('Board', BoardSchema);
