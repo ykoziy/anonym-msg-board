9d8cb3eabcde1fe650543ba07ab4d21a4dcc6ea0
Yuriy Koziy <yukoziy@gmail.com>
2019-12-13 12:28:52 -0500

    Board model, added validation.

--- models/Board.js
+++ models/Board.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let BoardSchema = new Schema({
-  board_title:     String,
-  threads:  [{type: Schema.Types.ObjectId, ref: 'Thread'}]
+  board_title:  {type: String, required: true},
+  threads:      [{type: Schema.Types.ObjectId, ref: 'Thread'}]
 });
 
 module.exports = mongoose.model('Board', BoardSchema);
